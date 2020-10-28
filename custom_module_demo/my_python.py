9cd395ca829627586a5497f77508a6255e51bfec
Ruben <rubendario.novelli@gmail.com>
2020-10-28 11:03:39 +0100

    new demo

---
+++ custom_module_demo/my_python.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+from ansible.module_utils.basic import *
+import os, json
+import re, sys
+
+if __name__ == '__main__':
+  fields = {
+  "yourName": {"required": True, "type": "str"}
+  }
+  module = AnsibleModule(argument_spec=fields)
+  yourName = os.path.expanduser(module.params['yourName'])
+  output = "Hola soy el custom module de python " + yourName
+  module.exit_json(msg=output)
